81d17298bd66b4bedcdda7e9d77b347955925fa0
Joseph Joyce <joej326@gmail.com>
2017-10-29 12:07:23 -0700

    finished aside from filter pipe

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filterString: string): any {
+    if(value.length === 0){
+      return value;
+    }
+    for(const item of value){
+      
+    }
+  }
+
+}
